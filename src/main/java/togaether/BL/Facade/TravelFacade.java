a3056b75c26fcb9211de2a74cea63cb873eb945c
Laura Benaiton <laurabenaiton@gmail.com>
2022-12-28 16:21:33 +0100

    Avancement TravelFacade (Laura)

--- src/main/java/togaether/BL/Facade/TravelFacade.java
+++ src/main/java/togaether/BL/Facade/TravelFacade.java
@@ -1,26 +1,125 @@
 package togaether.BL.Facade;
 
+import togaether.BL.Model.Message;
+import togaether.BL.Model.Notification;
 import togaether.BL.Model.Travel;
+import togaether.BL.Model.User;
+import togaether.DB.*;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public class TravelFacade {
 
-    static private Travel travel = null;
+    private Travel travel = null;
 
-    static private String collaborator = null;
+    private String collaborator = null;
 
-    static private List<String> collaborators = null;
+    private List<String> collaborators = null;
 
-    public static Travel getTravel() {
+    static private TravelFacade instance = new TravelFacade();
+
+    public static TravelFacade createInstance() {
+        return instance;
+    }
+
+    public Travel getTravel() {
         return travel;
     }
 
-    public static String getCollaborator() {
+    public void setTravel(Travel travel) {
+        this.travel = travel;
+    }
+
+    public String getCollaborator() {
         return collaborator;
     }
 
-    public static List<String> getCollaborators() {
+    public void setCollaborator(String collaborator) {
+        this.collaborator = collaborator;
+    }
+
+    public List<String> getCollaborators() {
         return collaborators;
+    }
+
+    public void setCollaborators(List<String> collaborators) {
+        this.collaborators = collaborators;
+    }
+
+    /**
+     * Query the archiving of the actual travel in DB
+     */
+    public void archiveTravel(Travel travel) throws DBNotFoundException {
+        AbstractFactory fact = AbstractFactory.createInstance();
+        TravelDAO travelDB = fact.getTravelDAO();
+        try {
+            travelDB.archiveTravel(travel);
+        } catch (SQLException e) {
+            System.out.println(e);
+            throw new DBNotFoundException();
+        }
+    }
+
+    /**
+     * Query the unarchiving of the actual travel in DB
+     */
+    public void unarchiveTravel(Travel travel) throws DBNotFoundException {
+        AbstractFactory fact = AbstractFactory.createInstance();
+        TravelDAO travelDB = fact.getTravelDAO();
+        try {
+            travelDB.unarchiveTravel(travel);
+        } catch (SQLException e) {
+            System.out.println(e);
+            throw new DBNotFoundException();
+        }
+    }
+
+    /**
+     * Query the supression of a specific travel (given in parameters) in DB
+     * @param travel
+     */
+    public void createTravel(Travel travel) throws DBNotFoundException{
+        AbstractFactory fact = AbstractFactory.createInstance();
+        TravelDAO travelDB = fact.getTravelDAO();
+
+        try {
+            travelDB.createTravel(travel);
+        } catch (SQLException e) {
+            System.out.println(e);
+            throw new DBNotFoundException();
+        }
+    }
+
+    /**
+     * Query the supression of a specific travel (given in parameters) in DB
+     * @param travel
+     */
+    public void deleteTravel(Travel travel) throws DBNotFoundException{
+        AbstractFactory fact = AbstractFactory.createInstance();
+        TravelDAO travelDB = fact.getTravelDAO();
+
+        try {
+            travelDB.deleteTravelById(travel.getIdTravel());
+        } catch (SQLException e) {
+            System.out.println(e);
+            throw new DBNotFoundException();
+        }
+    }
+
+    /**
+     * Query the creation of a specific notification (given in parameters) in DB
+     * @param notification
+     */
+    // ou créer la notif en fonction du voyage actuel et non du paramètre notification
+    public void createNotification(Notification notification){
+        AbstractFactory fact = AbstractFactory.createInstance();
+        NotificationDAO notificationDB = fact.getNotificationDAO();
+
+        try{
+            notificationDB.createNotification(notification);
+        }catch(SQLException e){
+            System.out.println(e);
+        }
     }
 }
