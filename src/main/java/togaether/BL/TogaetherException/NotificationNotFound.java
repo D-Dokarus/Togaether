32d6477c3332439771a406a5de1d686731f7d1b1
Maxime Stefani <max341@free.fr>
2023-01-10 22:06:15 +0100

    Modify Friends

---
+++ src/main/java/togaether/BL/TogaetherException/NotificationNotFound.java
@@ -0,0 +1,6 @@
+package togaether.BL.TogaetherException;
+
+public class NotificationNotFound extends Exception{
+    public NotificationNotFound(){ super();}
+    public NotificationNotFound(String e){ super(e);}
+}
